ea9ee1cdc3487ef77aa54172424803cfcbeb29b7
icaroxavier <icarogabriel999@icloud.com>
2024-02-02 07:12:31 -0300

    att

--- packages/react/src/types/styled.d.ts
+++ packages/react/src/types/styled.d.ts
@@ -1,5 +1,5 @@
 import 'styled-components'
-import { ThemeType } from '@nowds/tokens'
+import { ThemeType } from '@nowds/tokens/src/types'
 declare module 'styled-components' {
   export interface DefaultTheme extends ThemeType, ThemeType {}
 }
