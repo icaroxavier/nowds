79b20771e62aff4ccfc94496335a6fdd5d4db135
icaroxavier <icarogabriel999@icloud.com>
2024-02-05 15:20:00 -0300

    att

--- packages/react/src/components/Title.tsx
+++ packages/react/src/components/Title.tsx
@@ -3,6 +3,9 @@ import styled from 'styled-components'
 export const Text = styled.h1`
   color: ${({ theme }) => theme.colors.text};
   font-family: ${({ theme }) => theme.fonts.title};
+  font-size: ${({ theme }) => theme.fontSizes.xl};
+  line-height: ${({ theme }) => theme.lineHeights.default};
+  font-weight: ${({ theme }) => theme.fontWeights.normal};
 `
 
 export type TextProps = React.ComponentProps<typeof Text>
--- packages/tokens/src/fontSizes.ts
+++ packages/tokens/src/fontSizes.ts
@@ -2,7 +2,10 @@ export const fontSizes = {
   small: '0.75rem',
   medium: '0.875rem',
   large: '1rem',
-  xl: '1.5rem',
+  xl: '1.25rem',
+  '2xl': '1.5rem',
+  '3xl': '1.75rem',
+  '4xl': '2rem',
 } as const
 
 export type FontSizes = typeof fontSizes
